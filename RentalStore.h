dddf0ac9c8d41bd6da8633a7c5208a1fd95ec6bf
Tina <Tina.Fedorov@gmail.com>
2019-03-06 13:09:17 -0800

    Update RentalStore.h
    Updated rental store class

---
+++ RentalStore.h
@@ -0,0 +1,13 @@
+#include <fstream>
+
+class RentalStore {
+private:
+  DVD storage;
+  Hashtable<Customer> customerList;
+
+public:
+  void readTransactions( Fstream );
+
+  //print inventory should be in inventory class 
+
+}
