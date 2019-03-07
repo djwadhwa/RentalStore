4bc15352c0f6c86e010f0f1b10c032d464eac623
DJ <wadhwadhananjay@gmail.com>
2019-03-06 17:50:31 -0800

    updated spacing and added semicolon to the end of class decleration

--- Customer.h
+++ Customer.h
@@ -3,16 +3,16 @@
 #include <string>
 #include <iostream>
 
-class Customer {
+class Customer
+{
 public:
   std::string getFirstName();
   std::string getLastName();
   std::string getID();
-
   void printHistory();
+
 private:
   int ID;
   std::string firstName, lastName;
-
-}
+};
 #endif
