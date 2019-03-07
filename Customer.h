9ccfba8a9fcd5e2af8c8dfa77e93d2904d6aea7a
DJ <wadhwadhananjay@gmail.com>
2019-03-06 17:10:51 -0800

    Update Customer.h
    added getter methods and data

---
+++ Customer.h
@@ -0,0 +1,18 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#include <string>
+#include <iostream>
+
+class Customer {
+public:
+  std::string getFirstName();
+  std::string getLastName();
+  std::string getID();
+
+  void printHistory();
+private:
+  int ID;
+  std::string firstName, lastName;
+
+}
+#endif
