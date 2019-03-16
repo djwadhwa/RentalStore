628c80a27b74a7ea8b9cf28e7ba1c2eb7b8f124c
tarcisiusdaniel <46510062+tarcisiusdaniel@users.noreply.github.com>
2019-03-16 12:07:27 -0700

    Update HashTable.h

--- HashTesting/HashTable.h
+++ HashTesting/HashTable.h
@@ -14,16 +14,14 @@ public:
 
 
 private:
-  //ItemType* hashTable;
-  //int size;
   struct HashNode {
     int key;
     ItemType value;
     HashNode* next; //maybe needs struct infront?
   };
-
-  ItemType hashtable[100];  //make 100 constant var
-
+  HashNode* arrayHead;
+  HashNode** hashTable;  //make 100 constant var
+  const int TABLE_SIZE = 101; //default size of the array
 };
 
 #include "HashTable.cpp"
