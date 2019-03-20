e662975313541ba92f62bbe0d173684bef133449
tarcisiusdaniel <46510062+tarcisiusdaniel@users.noreply.github.com>
2019-03-20 13:45:10 -0700

    Update Inventory.h

---
+++ Inventory.h
@@ -0,0 +1,22 @@
+#ifndef DVDINVENTORY_H
+#define DVDINVENTORY_H
+#include <iostream>
+#include <string>
+#include "Inventory.h"
+#include "BinTree.h"
+using namespace std;
+
+class DVDInventory : public Inventory
+{
+    public:
+        DVDInventory();
+        ~DVDInventory();
+        void borrowItem(char ,string ,string );
+        void returnItem(char ,string ,string );
+        void printInventory();
+    private:
+        //BinTree<Comedy> comedyDVDList;
+        //BinTree<Classic> classicDVDList;
+        //BinTree<Drama> dramaDVDList;
+};
+#endif
