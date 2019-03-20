c370723067cf764bb9b61f44588f018a468e5d99
Tina <Tina.Fedorov@gmail.com>
2019-03-20 12:18:53 -0700

    Main driver

--- Driver.cpp
+++ Driver.cpp
@@ -1,12 +1,13 @@
 #include "RentalStore.h"
-#include "Inventory.h"  //or DVDinventory.h?
+//#include "Inventory.h"  //or DVDinventory.h?
 #include "Customer.h"
-#include "HashTable.h"
-#include "movie.h"
+//#include "HashTable.h"
+//#include "Movie.h"
 
 int main() {
 
   //Open txt file and Read in Inventory Object
+/*
   ifstream infile1("data4movies.txt");
   if (!infile1) {
     return 1;
@@ -15,14 +16,29 @@ int main() {
 
 //or DVDinventory
   Inventory.fillInventory(infile1);
-
+*/
   //Open txt file and read in Customer object OR do it in Rental store
   ifstream infile2("data4customers.txt");
   if (!infile2) {
     return 1;
+    std::cout << "error" << std::endl;
     //error msg?
   }
+//  infile2.close();
   //Create RentalStore
+  RentalStore store;
+  store.readInCustomerList(infile2); //eventually in constructor
+  //store.printCustomerList();
+  
+
+  ifstream infile3("data4commands.txt");
+  if (!infile3) {
+    return 1;
+    std::cout << "error3" << std::endl;
+    //error msg?
+  }
+
+  store.readTransactions(infile3);
 
   return 0;
-}
+};
