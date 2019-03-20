41eba5d5a91dc6f13336cfc1c0bd4d5cd1ccec11
Tina <Tina.Fedorov@gmail.com>
2019-03-20 12:09:53 -0700

    Rental Store update

--- RentalStore.cpp
+++ RentalStore.cpp
@@ -1,48 +1,81 @@
 #include "RentalStore.h"
 
 RentalStore::RentalStore() {
+  std::cout << "constructed" << std::endl;
 
 }
-
+/*
 RentalStore::RentalStore( Inventory inventoryObj, fstream infile ) {
   //copy inventoryObj to the inventory in RentalStore
-
+  DVDinventory = inventoryObj;
   //read in customers from file, or option 2: copy obj
+  readInCustomerList(infile);
+}
+*/
+
+void RentalStore::readTransactions( ifstream& infile ) {
+  //declare variables:
+  char action, itemType, genre; int custID; std::string attr1, attr2;
+  Customer* cust = NULL; //one customer per action
+  //read in the action
+  while ( infile >> action ) {
+    switch (action) {
+      case 'B':   //borrow item
+          std::cout << "borrow item" << std::endl;
+          infile >> custID >> itemType >> genre >> custID >> attr1 >> attr2;
+          //bool valid = DVDinventory.borrowItem( genre, attr1, attr2 ); //change stock #
+          //get Movie object using ID
+          //add borrow transaction to movie object
+          //moviePointer->addTransaction('B', itemType, genre, attr1, attr2);
+          // or have addBorrowTransaction method to Customer class
+          cust = customerList.get(custID);
+          std::cout << "customer: " << cust->getID() << std::endl;
+          break;
+      case 'R':   //return item
+          std::cout << "return item" << std::endl;
+          infile >> custID >> itemType >> genre >> custID >> attr1 >> attr2;
+          //bool valid = DVDinventory.returnItem( genre, attr1, attr2 ); //change stock #
+          //get Customer pointer using ID
+          //add borrow transaction to movie object
+          //moviePointer->addTransaction('R', itemType, genre, attr1, attr2);
+          // or have addReturnTransaction method to Customer class
+          cust = customerList.get(custID);
+          std::cout << "customer: " << cust->getID() << std::endl;
+          break;
+      case 'H':   //print transaction history of customer
+          std::cout << "print History" << std::endl;
+          infile >> custID;
+          //get Customer obj pointer
+          cust = get( custID );
+          if (cust != NULL)    //check if cust exists
+            std::cout << "customer: " << cust->getID() << std::endl;
+            //cust->printHistory();
+          break;
+      case 'I':   //print store inventory
+          std::cout << "print Inventory" << std::endl;
+          //DVDinventory.printInventory();
+          break;
+      default:
+        //if invalid action, error message?
+        std::cout << "invalid action" << std::endl;
+    }
+  }
 }
 
-void RentalStore::readTransactions( fstream infile ) {
-  //declare variables:
-  char action, itemType, genre; int custID; string attr1, attr2;
-  //read in the action
-  infile >> action;
-  switch (action) {
-    case 'B':   //borrow item
-        infile >> custID >> itemType >> genre >> custID >> attr1 >> attr2;
-        bool valid = DVDinventory.borrowItem( genre, attr1, attr2 ); //change stock #
-        //get Movie object using ID
-        //add borrow transaction to movie object
-        moviePointer->addTransaction('B', itemType, genre, attr1, attr2);
-        // or have addBorrowTransaction method to Customer class
-        break;
-    case 'R':   //return item
-        infile >> custID >> itemType >> genre >> custID >> attr1 >> attr2;
-        bool valid = DVDinventory.returnItem( genre, attr1, attr2 ); //change stock #
-        //get Customer pointer using ID
-        //add borrow transaction to movie object
-        moviePointer->addTransaction('R', itemType, genre, attr1, attr2);
-        // or have addReturnTransaction method to Customer class
-        break;
-    case 'H':   //print transaction history of customer
-        infile >> custID;
-        //get Customer obj pointer
-        Customer* ptr = get( custID );
-        if (ptr != NULL)    //check if cust exists
-          ptr->printHistory();
-        break;
-    case 'I':   //print store inventory
-        DVDinventory.printInventory();
-        break;
-    default:
-      //if invalid action, error message?
+
+//completed
+void RentalStore::readInCustomerList(ifstream& infile) {
+
+  int id; std::string firstName, lastName;
+  while ( infile >> id >> firstName >> lastName ) {
+    std::cout << id << firstName << lastName;
+    Customer newCustomer(id, firstName, lastName);
+    customerList.insert(id, newCustomer);
   }
+
+}
+
+//temporary, replace with method from customer class? or make my own?
+void RentalStore::printCustomerList() {
+  customerList.printTable();
 }
--- RentalStore.h
+++ RentalStore.h
@@ -2,18 +2,25 @@
 #define RENTALSTORE_H
 #include <iostream>
 #include <fstream>
-using namespace std;
+#include <string>
+#include "HashTable.h"
+#include "Customer.h"
+//#include "Inventory.h"
+//using namespace std;
 
 class RentalStore {
 private:
-  DVD DVDinventory;  //should be Inventory or DVD?
-  Hashtable<Customer> customerList;
+  //DVD DVDinventory;  //should be Inventory or DVD?
+  HashTable<Customer> customerList;
 
 public:
   RentalStore();
-  RentalStore( Inventory, fstream ); //or just create customer object beforehand,and make copy constr in customer
-  void readTransactions( fstream );
-
+//  RentalStore( Inventory, fstream ); //or just create customer object beforehand,and make copy constr in customer
+  void readTransactions( ifstream& );
+  void readInCustomerList( ifstream& );
+  void printCustomerList();
   //print inventory should be in inventory class
 
-}
+};
+
+#endif
