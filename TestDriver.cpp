f61b1ffc164cd0731a4175cd36fb9fdfd02de987
Tina <Tina.Fedorov@gmail.com>
2019-03-20 23:48:35 -0700

    td

--- TestDriver.cpp
+++ TestDriver.cpp
@@ -3,13 +3,27 @@
 //#include <string>
 
 int main() {
-
+/*
   std::string s1 = "John";
   std::string s2 = "Doe";
   int id = 1234;
   HashTable<Customer> h;
   Customer John( id, s1, s2 );
   h.insert( 1234, John );
+  Customer* c1 = h.get(1234);
+
+  //Customer c2(1250, "Dan", "Fun");
+  //c1 = c2;
+  c1->setFirstName("Dan");
+  //c1 = h.get(1234);
+  //c2 = h.get(1250);
+  Customer* c2 = h.get(1234);
+  std::cout << *c1 << std::endl;
+  std::cout << *c2 << std::endl;
+  //Customer c3 = h.get(1234);
+*/
+
+
 
 /*
   HashTable<int> j;
@@ -19,7 +33,7 @@ int main() {
 
   j.printTable();
 
-  std::cout << j.getItem(1234);
+  std::cout << j.get(1234);
 */
   return 0;
 }
