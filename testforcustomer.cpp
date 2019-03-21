6e6363ff0c75d2afc13206989b8d48c95899fa7e
tarcisiusdaniel <tarcih@uw.edu>
2019-03-20 17:02:12 -0700

    asd

--- testforcustomer.cpp
+++ testforcustomer.cpp
@@ -6,6 +6,8 @@ int main()
 {
     Customer c1(1, "Tarcisius", "Daniel"), c2(2, "DJ", "Wadhwa");
     c1.addHistory('B', 'D', "Pirates of Caribbeans");
+    c1.addHistory('B', 'D', "Kungfu Panda");
+    c1.addHistory('R', 'D', "Pirates of Caribbeans");
     c1.addHistory('R', 'D', "Kungfu Panda");
     c1.printHistory();
     return 0;
