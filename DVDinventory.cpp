88a95a2dd1709286e336a50e26efa30eaa0031b5
DJ <wadhwadhananjay@gmail.com>
2019-03-20 19:22:34 -0700

    Update DVDinventory.cpp

--- DVDinventory.cpp
+++ DVDinventory.cpp
@@ -23,24 +23,23 @@ void DVDInventory::borrowDVD(char DVDType, string firstAttribute, string secondA
 {
   if (DVDType == 'D')
   {
-    std::set<Drama>::iterator it;
+std::set<Drama>::iterator it;
     Drama d;
     d.setDirector(firstAttribute);
     d.setTitle(secondAttribute);
     it = dramaDVDList.find(d);
     *it.setStock((*it).getStock()-1);
   }
-  else if (DVDType == 'F')
+  else if (DVDType = 'F')
   {
-    //find the exact movie from 
-        std::set<Comedy>::iterator it;
+    std::set<Comedy>::iterator it;
         Comedy f;
         f.setTitle(secondAttribute);
         f.setYear (stoi(secondAttribute));
         it = comedyDVDList.find(f);
         *it.setStock((*it).getStock()-1);
   }
-  else if (DVDType == 'C')
+  else if (DVDType = 'C')
   {
     // std::set<Classic>::iterator it;
     //     Classic c;
@@ -55,23 +54,13 @@ void DVDInventory::returnDVD(char DVDType, string firstAttribute, string secondA
 {
   if (DVDType == 'D')
   {
-    std::set<Drama>::iterator it;
-    Drama d;
-    d.setDirector(firstAttribute);
-    d.setTitle(secondAttribute);
-    it = dramaDVDList.find(d);
-    *it.setStock((*it).getStock()+1);
+
   }
-  else if (DVDType == 'F')
+  else if (DVDType = 'F')
   {
-    std::set<Comedy>::iterator it;
-    Comedy f;
-    f.setTitle(secondAttribute);
-    f.setYear (stoi(secondAttribute));
-    it = comedyDVDList.find(f);
-    *it.setStock((*it).getStock()+1);
+
   }
-  else if (DVDType == 'C')
+  else if (DVDType = 'C')
   {
 
   }
@@ -96,7 +85,7 @@ void DVDInventory::printInventory()
     }
 }
 
-void DVDInventory::readInventory(ifstream &infile)
+void DVDInventory::readInventory (ifstream &infile)
 {
   while (!infile.eof())
   {
@@ -106,38 +95,63 @@ void DVDInventory::readInventory(ifstream &infile)
       stringstream s (input);
       string intermediate;
 
-      while(getline(s, intermediate, ','))
-          {
-              tokens.push_back(intermediate);
-          }
+      while (!infile.eof())
+      {
+          string input;
+          getline(infile, input);
 
-        if (tokens.size()!= 0)
-        {
-          if (tokens[0] == "D")
-          {
-            Drama dramaMovie;
-            dramaMovie.setStock(stoi(tokens[1]));
-            dramaMovie.setDirector(tokens[2]);
-            dramaMovie.setTitle(tokens[3]);
-            dramaMovie.setYear(stoi(tokens[4]));
-            std::cout << dramaMovie.getTitle() << '\n';
-            dramaDVDList.insert(dramaMovie);
-          }
-          if (tokens[0] == "F")
-          {
-            Comedy comedyMovie;
-            comedyMovie.setStock(stoi(tokens[1]));
-            comedyMovie.setDirector(tokens[2]);
-            comedyMovie.setTitle(tokens[3]);
-            comedyMovie.setYear(stoi(tokens[4]));
-            comedyDVDList.insert(comedyMovie);
-          }
-          if (tokens[0] == "C")
-          {
-            Classic classicMovie;
-            classicMovie.setStock(stoi(tokens[1]));
-            classicMovie.setDirector(tokens[2]);
-            classicMovie.setTitle(tokens[3]);
-          }
+          std::vector <string> tokens;
+          stringstream s (input);
+          string intermediate;
+
+          while(getline(s, intermediate, ','))
+              {
+                  tokens.push_back(intermediate);
+              }
+
+            if (tokens.size()!= 0)
+            {
+              if (tokens[0] == "D")
+              {
+                Drama dramaMovie;
+                dramaMovie.setStock(stoi(tokens[1]));
+                dramaMovie.setDirector(tokens[2].substr(1));
+                dramaMovie.setTitle(tokens[3].substr(1));
+                dramaMovie.setYear(stoi(tokens[4]));
+                D.insert(dramaMovie);
+              }
+              else if (tokens[0] == "F")
+              {
+                Comedy comedyMovie;
+                comedyMovie.setStock(stoi(tokens[1]));
+                comedyMovie.setDirector(tokens[2].substr(1));
+                comedyMovie.setTitle(tokens[3].substr(1));
+                comedyMovie.setYear(stoi(tokens[4]));
+                F.insert(comedyMovie);
+              }
+              else if (tokens[0] == "C")
+              {
+                Classic classicMovie;
+                classicMovie.setStock(stoi(tokens[1]));
+                classicMovie.setDirector(tokens[2].substr(1));
+                classicMovie.setTitle(tokens[3].substr(1));
+
+                stringstream s2 (tokens[4]);
+                std::vector<string> tokens2;
+
+                while(getline(s2, intermediate, ' '))
+                    {
+                        tokens2.push_back(intermediate);
+                    }
+                classicMovie.setMajorActor(tokens2[1]+" "+tokens2[2]);
+                classicMovie.setReleaseMonth(stoi(tokens2[3]));
+                classicMovie.setYear(stoi(tokens2[4]));
+                C.insert (classicMovie);
+              }
+              else
+              {
+                  std::cout << "Incorrect code: " << tokens[0] << '\n';
+              }
+            }
         }
 }
