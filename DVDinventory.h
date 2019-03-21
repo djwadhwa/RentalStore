796fc116365c330fff75d837f9838c7d83fc6e73
tarcisiusdaniel <tarcih@uw.edu>
2019-03-20 17:04:46 -0700

    change on DVDInventory.h

--- DVDinventory.h
+++ DVDinventory.h
@@ -10,14 +10,14 @@ class DVDInventory : public Inventory
     public:
         DVDInventory();
         ~DVDInventory();
-        void borrowItem(char ,string ,string );
-        void returnItem(char ,string ,string );
+        void borrowDVD(char ,string ,string );
+        void returnDVD(char ,string ,string );
         void printInventory();
         void readInventory(ifstream);
 
     private:
         set<Comedy> comedyDVDList;
-        // set<Classic> classicDVDList;
+        set<Classic> classicDVDList;
         set<Drama> dramaDVDList;
 };
 #endif
